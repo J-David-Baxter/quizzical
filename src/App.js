8205423a44c0d4fa053ce7806badc5cac5372d5d
J-David-Baxter <jdavidbx@gmail.com>
2023-03-28 14:49:02 -0400

    Create pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Home from './pages/Home/Home';
 import './App.css';
+import { useState } from 'react';
+import Quiz from './pages/Quiz/Quiz';
 
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  
   return (
-    <div className="App">App</div>
+    <div className="App">
+      {!isPlaying ? 
+        <Home /> :
+        <Quiz />
+      }
+    </div>
   );
 }
 
