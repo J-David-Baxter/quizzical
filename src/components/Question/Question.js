558f1eb8d55e6a5efec78d4bfa0c432872b27c00
J-David-Baxter <jdavidbx@gmail.com>
2023-03-29 14:25:45 -0400

    Change answer state on quiz submit

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -4,9 +4,18 @@ import Answer from '../Answer/Answer';
 import './Question.css';
 
 const Question = ({ question, correctAnswer, incorrectAnswers, setQuizScore, quizIsSubmitted }) => {
-  let allAnswers = incorrectAnswers.concat(correctAnswer);
-  shuffleAnswers(allAnswers);
-  let answerArray = allAnswers.map((answer, i) => {
+  const [answers, setAnswers] = useState([]);
+
+  function toggleSelected(index) {
+    setAnswers(prev => prev.map((answer, i) => {
+      return i === index ? {...answer, isSelected: !answer.isSelected} : {...answer, isSelected: false}
+    }))
+  }
+
+  useEffect(() => {
+    let allAnswers = incorrectAnswers.concat(correctAnswer);
+    shuffleAnswers(allAnswers);
+    let answerArray = allAnswers.map((answer, i) => {
     return {
       text: answer,
       isSelected: false,
@@ -16,20 +25,23 @@ const Question = ({ question, correctAnswer, incorrectAnswers, setQuizScore, qui
       gray: false
     }
   })
-
-  const [answers, setAnswers] = useState([]);
-
-  function toggleSelected(index) {
-    setAnswers(prev => prev.map((answer, i) => {
-      return i === index ? {...answer, isSelected: true} : {...answer, isSelected: false}
-    }))
-  }
-
-
+    setAnswers(answerArray)
+  }, [question, correctAnswer, incorrectAnswers])
 
   useEffect(() => {
-    setAnswers(answerArray)
-  }, [question])
+     setAnswers(prev => prev.map(answer => {
+      if (answer.isSelected && !answer.isCorrect) {
+        return {...answer, isSelected: false, red: true}
+      } else if (answer.isSelected && answer.isCorrect) {
+        setQuizScore(prev => prev + 1)
+        return {...answer, isSelected: false, green: true}
+      } else if (!answer.isSelected && answer.isCorrect) {
+        return {...answer, isSelected: false, green: true}
+      } else {
+        return {...answer, isSelected: false, gray: true}
+      }
+     }))
+  }, [quizIsSubmitted, setQuizScore])
 
   return (
     <div className='Question'>
