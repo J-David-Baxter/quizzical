0bcbdae59ac6ce503adb4e8ed4a4fda4c6fd347b
J-David-Baxter <jdavidbx@gmail.com>
2023-04-04 16:09:04 -0400

    Change home page description

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,7 @@ const Home = ({ setIsPlaying }) => {
   return (
     <div className='home--left'>
         <h1 className='home--title'>Quizzical</h1>
-        <p className='home--text'>Take a multiple choice quiz!</p>
+        <p className='home--text'>Take a customized quiz!</p>
         <button className='home--btn' onClick={() => setIsPlaying(true)}>Start quiz</button>
     </div>
   )
